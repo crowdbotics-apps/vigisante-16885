cc4c58805f99bd38e6003b3e4e633a07dd8b2f73
Crowdbotics <team@crowdbotics.com>
2020-05-11 22:30:47 +0000

    Update models

--- vigisante_16885/wsgi.py
+++ vigisante_16885/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vigisante_16885.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vigisante_16885.settings")
 
 application = get_wsgi_application()
